baa31dec1d48660ff530807f64bda3994929466b
kyle11235 <you@example.com>
2021-04-26 20:20:58 +0800

    clear

---
+++ build/CMakeFiles/md5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/md5.dir/md5.cpp.o"
+  "CMakeFiles/md5.dir/md5.cpp.o.d"
+  "bin/md5"
+  "bin/md5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/md5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
