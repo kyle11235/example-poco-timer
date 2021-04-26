7aa8ed07ad9afd16704243756158a20beb43086e
someone <someone@example.com>
2021-04-26 20:05:38 +0800

    clear

--- cli_conan_build.sh
+++ cli_conan_build.sh
@@ -1,23 +1,25 @@
 BUILD_NAME=cli-conan-build
 BUILD_NUMBER=1
 
-cd build
+rm -rf build
+mkdir build
+pushd build
 
-# 1. resolve
-conan install .. -r demo-conan-virtual --build missing
+# 1. remove conan cache
+# rm -rf ~/.conan/data
 
-# 2. build
+# 2. resolve
+conan install .. --build missing
+# conan install .. -r demo-conan-virtual --build missing
+
+# 3. build
 cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 
-# 2. mock build by copy
+# 4. upload a lib
 # cp -r ~/.conan/data/zlib .
+# conan upload zlib/1.2.11@conan/stable -r demo-conan-local --all
 
-# 3. upload
-conan upload zlib/1.2.11@conan/stable -r demo-conan-local --all
-
-# 4. publish build info
+# 5. publish build info
 jfrog rt bad $BUILD_NAME $BUILD_NUMBER "../conanbuildinfo.txt"
 jfrog rt bp $BUILD_NAME $BUILD_NUMBER
-
-cd ../
--- install.sh
+++
@@ -1,1 +0,0 @@
-conan install . -r demo-conan-virtual  --build missing
